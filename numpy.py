0c74c992327dcbc832b244ff6b87e8cfa01c149d
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2025-08-06 14:01:00 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,12 +1,16 @@
 # Processing time of numpy and normal call
+import numpy as np
 from time import process_time
-a = [1,2,3,4,5,6]
+list_1 = [i for i in range(2000)]
 print('with out numpy')
 start = process_time()
-print(a)
+list_1 = [i+2 for i in list_1] 
 end = process_time()
 print(end - start)
 
-import numpy as np
-a = [1,2,3,4,5,6,]
-print(a)
+print('With numpy(libarary)')
+np_array = np.array([i for i in range(2000)])
+start_np = process_time()
+np_array = np_array+2
+end_np = process_time()
+print(end_np - start_np)
